ee38d377726cbc4285c1652cad7f78253d9aae67
Cameron <cameron.karasiuk@gmail.com>
2022-07-26 16:58:54 -0400

    getting apollo updated as well as auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,60 @@
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
 const resolvers = {
-    Query: {
-        helloWorld: () => {
-            return 'Hello world!';
-        }
-    }
+	Query: {
+		me: async (parent, args, context) => {
+			if (context.user) {
+				const userData = User.findOne({ _id: context.user._id }).select("-__v -password");
+				return userData;
+			}
+			throw new AuthenticationError("You need to be logged in!");
+		},
+	},
+
+	Mutation: {
+		addUser: async (parent, args) => {
+			const user = await User.create(args);
+			const token = signToken(user);
+			return { token, user };
+		},
+        login: async (parent, { email, password }, context) => {
+			const user = await User.findOne({ email });
+			if (!user) {
+				throw new AuthenticationError("Incorrect email or password");
+			}
+			const isValid = await user.isCorrectPassword(password);
+			if (!isValid) {
+				throw new AuthenticationError("Incorrect username or password");
+			}
+			const token = signToken(user);
+			return { token, user };
+		},
+
+		saveBook: async (parent, { bookData }, context) => {
+			if (context.user) {
+				const updatedUser = await User.findByIdAndUpdate(
+					{ _id: context.user._id },
+					{ $push: { savedBooks: bookData } },
+					{ new: true }
+				);
+				return updatedUser;
+			}
+		},
+
+		removeBook: async (parent, { bookId }, context) => {
+			if (context.user) {
+				const currentUser = await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{ $pull: { savedBooks: { bookId } } },
+					{ new: true }
+				);
+			return currentUser;
+			}
+            throw new AuthenticationError('You need to be logged in!');
+		},
+	},
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,42 @@
-// import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-// create our typeDefs
 const typeDefs = gql`
-    type Query {
-        helloWorld: String
-    }
+	type User {
+		_id: ID!
+		username: String!
+		email: String!
+		bookCount: Int
+		savedBooks: [Book]
+	}
+	type Book {
+		bookId: ID!
+		authors: [String]
+		description: String
+		image: String
+		link: String
+		title: String!
+	}
+	input BookInput {
+		authors: [String]
+		description: String!
+		bookId: String!
+		image: String
+		link: String
+		title: String!
+	}
+	type Auth {
+		token: ID!
+		user: User
+	}
+	type Query {
+		me: User
+	}
+	type Mutation {
+		addUser(username: String!, email: String!, password: String!): Auth
+		login(email: String!, password: String!): Auth
+		saveBook(bookData: BookInput!): User
+		removeBook(bookId: ID!): User
+	}
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
